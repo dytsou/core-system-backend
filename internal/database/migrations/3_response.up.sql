aafe1d147ea1c80d5f449de5fecee3e902674298
dytsou <d.y.tsou@gmail.com>
2025-07-19 16:35:08 +0800

    feat: add database migration scripts for responses and answers tables

---
+++ internal/database/migrations/3_response.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS responses (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    form_id UUID NOT NULL,
+    submitted_by UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS answers (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    response_id UUID NOT NULL REFERENCES responses(id) ON DELETE CASCADE,
+    question_id UUID NOT NULL,
+    type answer_type NOT NULL,
+    value VARCHAR(255) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
