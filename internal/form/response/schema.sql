aeee3b0bb96c46d3e2383c8220dad6bdced05911
dytsou <d.y.tsou@gmail.com>
2025-07-30 15:38:20 +0800

    refactor: update answers table schema to ensure proper foreign key references and change value type to TEXT for better flexibility

--- internal/form/response/schema.sql
+++ internal/form/response/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS answers (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     response_id UUID NOT NULL REFERENCES responses(id) ON DELETE CASCADE,
     question_id UUID NOT NULL REFERENCES questions(id) ON DELETE CASCADE,
-    type question_type NOT NULL REFERENCES questions(type) ON DELETE CASCADE,
+    type question_type NOT NULL,
     value TEXT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
