7d78fdc0f6fd92ca138f07bb21e5cb74c573b9d2
dytsou <d.y.tsou@gmail.com>
2025-09-30 01:05:31 +0800

    fix: update SQL queries to exclude archived inbox messages for user retrieval

--- internal/inbox/queries.sql
+++ internal/inbox/queries.sql
@@ -36,7 +36,7 @@ JOIN inbox_message im ON uim.message_id = im.id
 LEFT JOIN forms f ON im.type = 'form' AND im.content_id = f.id
 LEFT JOIN units u ON f.unit_id = u.id
 LEFT JOIN units o ON u.org_id = o.id
-WHERE uim.user_id = $1;
+WHERE uim.user_id = $1 AND uim.is_archived = false;
 
 -- name: UpdateByID :one
 UPDATE user_inbox_messages AS uim
