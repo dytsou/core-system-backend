8531a93349001a7d78c230a26c961b1fd81a3424
dytsou <d.y.tsou@gmail.com>
2025-10-13 03:17:06 +0800

    fix: rename email column in 10_emails.up.sql

--- internal/database/migrations/10_emails.up.sql
+++ internal/database/migrations/10_emails.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS emails (
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    email VARCHAR(255) NOT NULL,
+    value VARCHAR(255) NOT NULL,
     provider VARCHAR(255) NOT NULL, -- google, nycu, etc.
     provider_id VARCHAR(255) NOT NULL, 
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    UNIQUE(email, provider, provider_id)
+    UNIQUE(value, provider, provider_id)
 );
